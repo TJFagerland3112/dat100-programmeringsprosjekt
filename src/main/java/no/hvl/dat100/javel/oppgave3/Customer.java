9d154a4b56f7e8beab749eb5b0b7d3c36880c946
TJFagerland3112 <188135@stud.hvl.no>
2025-10-20 16:23:27 +0200

    oppgave 3 fullført

--- src/main/java/no/hvl/dat100/javel/oppgave3/Customer.java
+++ src/main/java/no/hvl/dat100/javel/oppgave3/Customer.java
@@ -1,16 +1,51 @@
 package no.hvl.dat100.javel.oppgave3;
 
 public class Customer {
-
-    // TODO - object variables (attributes)
+// objektvariabler
+        public String name;
+        public String email;
+        public int costumer_id;
+        public PowerAgreementType agreement;
 
     public Customer(String name, String email, int customer_id, PowerAgreementType agreement) {
-
-        // TODO
+        this.name = name;
+        this.email = email;
+        this.costumer_id = customer_id;
+        this.agreement = agreement;
+    }
+// get metoder
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public int getCustomer_id() {
+        return costumer_id;
+    }
+    public PowerAgreementType getAgreement() {
+        return agreement;
+    }
+// set metoder
+    public void setName(String name) {
+        this.name = name;
     }
 
-    // TODO - getter/setter methods for all objectvariables
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setCustomer_id(int costumer_id) {
+        this.costumer_id = costumer_id;
+    }
+    public void setAgreement(PowerAgreementType agreement) {
+        this.agreement = agreement;
+    }
 
-    // TODO - toString method
+// toString metode
+    public String toString() {
+        return "Costumer number: " + costumer_id +"\nnavn: " + name + "\nemail: " + email + "\nagreement: " + agreement;
+    }
+
+
 
 }
--- src/main/java/no/hvl/dat100/javel/oppgave3/CustomerMain.java
+++ src/main/java/no/hvl/dat100/javel/oppgave3/CustomerMain.java
@@ -11,9 +11,41 @@ public class CustomerMain {
         System.out.println("==============");
         System.out.println();
 
-        /*
-        TODO
 
+        Customer Customer1 = new Customer("Hans Hansen", "hansehans@gmail.com", 112, PowerAgreementType.SPOTPRICE);
+
+        //tester for get metoder
+        String name1 = Customer1.getName();
+        System.out.println("Name: " + name1);
+        String email1 = Customer1.getEmail();
+        System.out.println("Email: " + email1);
+        int c_id1 = Customer1.getCustomer_id();
+        System.out.println("Costumer ID: " + c_id1);
+        PowerAgreementType agreement1 = Customer1.getAgreement();
+        System.out.println("Agreement: " + agreement1);
+
+        System.out.println();
+        System.out.println("==============");
+        System.out.println();
+
+        //tester for set metoder
+        Customer1.setName("Hansmann");
+        System.out.println("Name: " + Customer1.getName());
+        Customer1.setEmail("haaaans@yahoomail.com");
+        System.out.println("Email: " + Customer1.getEmail());
+        Customer1.setCustomer_id(123);
+        System.out.println("Costumer ID: " + Customer1.getCustomer_id());
+        Customer1.setAgreement(PowerAgreementType.NORGESPRICE);
+        System.out.println("Agreement: " + Customer1.getAgreement());
+
+        System.out.println();
+        System.out.println("==============");
+        System.out.println();
+
+        String CustomerOutput = Customer1.toString();
+        System.out.println(CustomerOutput);
+
+        /*
          Write code that creates a Customer object and teste the methods implemented in the class
 
         */
